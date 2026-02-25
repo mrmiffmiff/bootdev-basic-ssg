7def910ef3f6a7e1f9611dc95edaeedb04651d20
Robert Eisenman <robert.t.eisenman@gmail.com>
2026-02-24 20:46:25 -0800

    build for gh pages

--- src/generate_page.py
+++ src/generate_page.py
@@ -2,12 +2,14 @@ import os
 from blocktype import markdown_to_html_node
 from extract_title import extract_title
 
-def generate_pages_recursive(dir_path_content: str, template_path: str, dest_dir_path: str):
+def generate_pages_recursive(dir_path_content: str, template_path: str, dest_dir_path: str, basepath: str):
     root = os.path.abspath(os.path.join(os.path.dirname(__file__), "../"))
     full_contents_path = os.path.join(root, dir_path_content)
     if not os.path.exists(full_contents_path):
         raise FileNotFoundError("Content path doesn't exist")
     full_dests_path = os.path.join(root, dest_dir_path)
+    if not os.path.exists(full_dests_path):
+        os.mkdir(full_dests_path)
     contents = os.listdir(full_contents_path)
     for path in contents:
         full_content_path = os.path.join(full_contents_path, path)
@@ -19,14 +21,14 @@ def generate_pages_recursive(dir_path_content: str, template_path: str, dest_dir
             dest_path = os.path.join(os.path.dirname(path), html_filename)
             full_dest_path = os.path.join(full_dests_path, dest_path)
             rel_dest_path = os.path.relpath(full_dest_path, root)
-            generate_page(rel_content_path, template_path, rel_dest_path)
+            generate_page(rel_content_path, template_path, rel_dest_path, basepath)
         else:
             full_dest_path = os.path.join(full_dests_path, path)
             os.mkdir(full_dest_path)
             rel_dest_path = os.path.relpath(full_dest_path, root)
-            generate_pages_recursive(rel_content_path, template_path, rel_dest_path)
+            generate_pages_recursive(rel_content_path, template_path, rel_dest_path, basepath)
 
-def generate_page(from_path: str, template_path: str, dest_path: str):
+def generate_page(from_path: str, template_path: str, dest_path: str, basepath: str):
     print(f"Generating a page from {from_path} to {dest_path} using {template_path}")
     root = os.path.abspath(os.path.join(os.path.dirname(__file__), "../"))
     full_from_path = os.path.join(root, from_path)
@@ -43,7 +45,8 @@ def generate_page(from_path: str, template_path: str, dest_path: str):
     surrounds = template_file.read()
     template_file.close()
     surrounds_with_title = surrounds.replace("{{ Title }}", title, 1)
-    final_html = surrounds_with_title.replace("{{ Content }}", content, 1)
+    surrounds_with_content = surrounds_with_title.replace("{{ Content }}", content, 1)
+    final_html = surrounds_with_content.replace('href="/', f'href="{basepath}').replace('src="/', f'src="{basepath}')
     full_dest_path = os.path.join(root, dest_path)
     dest_directory = os.path.dirname(full_dest_path)
     if not os.path.exists(dest_directory):
--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
-from textnode import TextNode, TextType
+import sys
 from copy_files import copy_files
 from generate_page import generate_pages_recursive
 
 def main():
-    copy_files("static", "public")
-    generate_pages_recursive("content", "template.html", "public")
+    basepath = sys.argv[1] if len(sys.argv) > 1 else "/"
+    copy_files("static", "docs")
+    generate_pages_recursive("content", "template.html", "docs", basepath)
 
 main()
